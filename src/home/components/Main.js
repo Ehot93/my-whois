51ff1307e96309a6f6fa669ea69e99efd0499bcb
Vlad <Vlad3x@mail.ru>
2019-10-24 16:31:20 +0700

    added get query

--- src/home/components/Main.js
+++ src/home/components/Main.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import React, {Component} from 'react'
 
 const Main = ()=>{
@@ -10,5 +9,23 @@ const Main = ()=>{
         </label>
         <input type="submit" value="Отправить" />
         </form>
-    )
+    );
 }
+
+let key = "5f0b4b5c53b8cb010899e937ac4cdad7e825247ed10b7598647f4b1fda931aff";
+var query = ()=>{
+    console.log('*');
+    var xhr = new XMLHttpRequest(),
+    temp = {"indicator": "pulsedive.com",
+    "pretty": "1",
+    "key": key}
+    //xhr.open(method, URL, async, user, password)
+    xhr.open('GET', 'https://pulsedive.com/api/info.php', false);
+    xhr.send(temp);
+    if (xhr.status != 200) {
+    alert( xhr.status + ': ' + xhr.statusText );
+    } else {
+    alert( xhr.responseText );
+}}
+ 
+export default Main;
