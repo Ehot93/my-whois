d262815605aa02e76dc540bb4ff35d901f0ea691
Vlad <Vlad3x@mail.ru>
2019-10-28 14:18:16 +0700

    v 0.2
    good get query

--- src/home/components/Main.js
+++ src/home/components/Main.js
@@ -1,66 +1,74 @@
 import React from 'react'
 
 const Main = ()=>{
+    
     return(
         <form>
         <label>
-        Имя:
-        <input type="text" name="name" />
+          Имя:
+          <input type="text" />
         </label>
         <input type="submit" value="Отправить" />
-        </form>
+      </form>
     );
-}
+    }
+    
+    var query = ()=>{
+      var xhr = new XMLHttpRequest(),
+      temp = {
+          "indicator": "pulsedive.com",
+          "pretty": "1",
+          "key": "c77b24f431e9a65f57b4504586e164b75f5c41490d87c44dc5f48fa290b18b07"
+      }
+      
+      xhr.open('GET', 'https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_F1M4RguTCYTxtOxwM7mBdI53RHqhP&domainName=google.com&outputFormat=JSON&callback=resp', true);
+      //xhr.open('GET', "https://pulsedive.com/api/info.php?indicator=amazon.com&key=c77b24f431e9a65f57b4504586e164b75f5c41490d87c44dc5f48fa290b18b07", true);
+      //xhr.setRequestHeader();
+      xhr.onload = function(e){
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            //xhr.send(temp);
+            console.log(xhr.responseText);
+          } else {
+            console.error(xhr.statusText);
+          }
+        }
+      };
+      xhr.onerror = function (e) {
+        console.error(xhr.statusText);
+      };
+      //xhr.responseType = "application/json";
+      //xhr.setRequestHeader("Content-type","application/json");
+      //console.log('state:' + xhr.onreadystatechange);
+      xhr.send(temp);
+      function respons(data){
+        console.log("data = " + data);
+        return data;
+      }
+    }
+   query();
+  
+  function mob(){
+  var scriptt = document.createElement("script");
+  var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+  scriptt.type = 'text/javascript';
+  //scriptt.src = "https://pulsedive.com/api/info.php?method=get&indicator=amazon.com&key=c77b24f431e9a65f57b4504586e164b75f5c41490d87c44dc5f48fa290b18b07?callback=response";// + callbackName;
+  //scriptt.src = "https://pulsedive.com/api/info.php?method=get&indicator=amazon.com&pretty=1&key=c77b24f431e9a65f57b4504586e164b75f5c41490d87c44dc5f48fa290b18b07?callback=" + callbackName;
+  //scriptt.src = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20130512T104455Z.8a0ed400b0d249ba.48af47e72f40c8991e4185556b825273d104af68&text=курица&callback=" + callbackName + "&lang=ru-en"
+  //scriptt.src = "http://htmlweb.ru/analiz/api.php?whois&url=htmlweb.ru&json"  + callbackName;
+  scriptt.src = "https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_F1M4RguTCYTxtOxwM7mBdI53RHqhP&domainName=amazon.com&outputFormat=JSON&callback" + callbackName;
+  document.body.appendChild(scriptt);
+  
+  window[callbackName] = function(data){
+  console.log('data = ' + data);    
+  document.body.removeChild(scriptt);
+  }
+  }
+  function response(out){
+  console.log('data = ' + out);
+  return out;
+  }
+  // mob();
+  console.log('load');
 
-//let key = "5f0b4b5c53b8cb010899e937ac4cdad7e825247ed10b7598647f4b1fda931aff";
-// let key ='c77b24f431e9a65f57b4504586e164b75f5c41490d87c44dc5f48fa290b18b07';
-// var query = ()=>{
-//     var xhr = new XMLHttpRequest(),
-//     temp = {
-//     "indicator": "github.com",
-//     "pretty": "1",
-//     "key": key}
-//     //xhr.open(method, URL, async, user, password)
-//     xhr.open('GET', 'https://pulsedive.com/api/info.php', true);
-//     console.log('state:' + xhr.onreadystatechange);
-//     // xhr.setRequestHeader("indicator", "github.com");
-//     // xhr.setRequestHeader("pretty", "1");
-//     // xhr.setRequestHeader("key", "c77b24f431e9a65f57b4504586e164b75f5c41490d87c44dc5f48fa290b18b07");
-    
-    
-    
-//     //xhr.getResponseHeader("GET");
-//     xhr.send(temp);
-    
-//     if (xhr.status != 200) {
-//         console.log( xhr.status + ': ' + xhr.statusText );
-//     } else {
-//         console.log( xhr.responseText );
-// }}
-// query();
-var tem;
-function mob(){
-var scriptt = document.createElement("script");
-var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-    scriptt.type = 'text/javascript';
-    //scriptt.src = "https://pulsedive.com/api/info.php?method=get&indicator=amazon.com&key=c77b24f431e9a65f57b4504586e164b75f5c41490d87c44dc5f48fa290b18b07?callback=response";// + callbackName;
-    //scriptt.src = "https://pulsedive.com/api/info.php?method=get&indicator=amazon.com&pretty=1&key=c77b24f431e9a65f57b4504586e164b75f5c41490d87c44dc5f48fa290b18b07?callback=" + callbackName;
-    //scriptt.src = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20130512T104455Z.8a0ed400b0d249ba.48af47e72f40c8991e4185556b825273d104af68&text=курица&callback=" + callbackName + "&lang=ru-en"
-    scriptt.src = "http://htmlweb.ru/analiz/api.php?whois&url=htmlweb.ru&json"  + callbackName;
-    document.body.appendChild(scriptt);
-    function response(out){
-      console.log('data = ' + out);
-      return out;
-    }
-    window[callbackName] = function(data){
-      console.log('data = ' + data);    
-      document.body.removeChild(scriptt);
-    }
-    }
-    function response(out){
-      console.log('data = ' + out);
-      return out;
-    }
-    mob();
- 
 export default Main;
